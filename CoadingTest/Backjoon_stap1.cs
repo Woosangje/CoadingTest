d2b9507d69e73e3f9d468e84a975711c9e6ac4e5
Woosangje <dnedrbs3@naver.com>
2021-11-29 21:04:19 +0900

    1129a

---
+++ CoadingTest/Backjoon_stap1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace CoadingTest
+{
+
+
+    //1 Hello Would 출력 https://www.acmicpc.net/step/1
+    //2 두 수를 입력 받고 5종류의 답을 출력하는 문제 a-b,a+b,a*b,a/b,a%b
+
+    //if문 예제 https://www.acmicpc.net/step/4
+    /* 연도가 주어졌을 때 윤년이면 1, 아니면 0을 출력하는 프로그램을 작성하시오.
+     윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때이다.
+     예를 들어, 2012년은 4의 배수이면서 100의 배수가 아니라서 윤년이다. 1900년은 100의 배수이고 
+         400의 배수는 아니기 때문에 윤년이 아니다. 
+         하지만, 2000년은 400의 배수이기 때문에 윤년이다.
+    */
+}
---
+++ CoadingTest/Programmers_lv1_1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace CoadingTest
+{
+
+
+    public class Solution
+    {
+        public int[] solution(int[] lottos, int[] win_nums)
+        {
+            int[] answer = new int[2];
+
+            int match_cnt = 0;
+            int zero_cnt = 0;
+
+            for(int i=0; i<lottos.length; i++)
+            {
+                //0인 개수 측정
+                if (lottos[i] == 0) zero_cnt++;
+                //win_nums 의 길이만큼 도는 inner for
+                for(int j=0; j<win_nums.length; j++)
+                {
+                    //일치하는 번호가 있는 경우 일치개수++
+                    if(win_nums[j] == lottos[i])
+                    {
+                        match_cnt++;
+                        break;
+                    }
+                }
+            }
+
+            //최고 순위 세팅
+            answer[0] = Math.min(7 - (zero_cnt + match_cnt), 6);
+            answer[1] = Math.min(7 - match_cnt, 6);
+
+            return answer;
+        }
+    }
+}
