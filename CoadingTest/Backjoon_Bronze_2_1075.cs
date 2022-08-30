ee35099a082f66b25739f48c72f181578f70d610
Woosangje <dnedrbs3@naver.com>
2022-08-30 23:07:08 +0900

    08/30a

--- CoadingTest/Backjoon_Bronze_2_1075.cs
+++ CoadingTest/Backjoon_Bronze_2_1075.cs
@@ -6,37 +6,38 @@ using static System.Console;
 namespace Backjoon_Bronze_2_1075 {
 
 
+
     /*
-  class MainApp {
+class MainApp {
 
-      static void Main(string[] args) {
+    static void Main(string[] args) {
 
 
-          int N = 1000;
-          int F = 3;
-          int result = GetLast2Dight();
+        int N = 1000;
+        int F = 3;
+        int result = GetLast2Dight();
 
-          if(result >= 0 && result < 10) {
+        if(result >= 0 && result < 10) {
 
-              Write("0");
-              WriteLine(result);
-          }
+            Write("0");
+            WriteLine(result);
+        }
 
-          int GetLast2Dight() {
+        int GetLast2Dight() {
 
-              //100의 자리 시작
-              int start = (N / 100) * 100;
-              int result = 0;
+            //100의 자리 시작
+            int start = (N / 100) * 100;
+            int result = 0;
 
-              for(int i= start; i < (start + 100); i++) {
+            for(int i= start; i < (start + 100); i++) {
 
-                  if(i% F == 0) {
-                      result = i % 100;
-                      break;
-                  }
-              }
-              return result;
-          }
-      }
-  }*/
+                if(i% F == 0) {
+                    result = i % 100;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}*/
 }
